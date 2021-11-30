031756e3b57ac15756ac33b2c34b103c00ab30b8
Enoch2k2 <enoch2k2@hotmail.com>
2021-11-30 11:00:33 -0600

    added routing, homepage, list page, create page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Navbar from './components/navigation/Navbar';
 import Home from './components/static/Home';
+import ListRecipes from './components/recipes/ListRecipes';
+import NewRecipe from './components/recipes/NewRecipe';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<ListRecipes />} />
+        <Route path="/recipes/new" element={<NewRecipe />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome To Foodies</h1>
+      
     </div>
   )
 }
